42d164ebae33a0d262589ad5cb25660c80a71a09
lekan012704 <durosinmiabdulsobuur>
2022-06-08 00:09:53 +0100

    second commit

--- Lekan Donation/Controllers/HomeController.cs
+++ Lekan Donation/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,9 +14,46 @@ namespace Lekan_Donation.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("durosinmiabdulsobuur@gmail.com", "Durosinmi Abdulsobuur");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "Yaseer0914";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
 
-       
 
-       
+
     }
 }
